66ce5286ec3fd306bb9892dba19d684eecf792ef
nosferatu2009 <51450808+nosferatu2009@users.noreply.github.com>
2024-01-08 00:10:40 +0530

    Add files via upload
    Reverse LinkedList Iterative approach

---
+++ ReverseLinkedList.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+
+class Node{
+    public :
+    int data;
+    Node* next;
+    
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+}
+
+Node* ReverseLinkedList(Node* prev,Node* curr){
+   
+    while(curr!=null){
+        Node* temp = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = temp;
+    }
+
+  return prev;
+}
+
+
+void printLinkedList(Node* head){
+    
+    Node* temp = head;
+    while(temp!=null){
+        cout<< temp->data<< "->" ;
+    }
+}
+
+
+int main() {
+  
+    Node n1 = new Node(10);
+    Node n2 = new Node(20);
+    Node n3 = new Node(30);
+    
+    n1->next = n2;
+    n2->next = n3;
+    
+    Node* r = ReverseLinkedList(NULL,n1);
+    printLinkedList(r);
+    
+  return 0;
+}
